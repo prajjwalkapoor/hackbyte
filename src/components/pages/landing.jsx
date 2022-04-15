6614b825c0a9156a5169e8b288ce0b70230362c9
= <=>
2022-04-15 20:44:54 +0530

    header

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { db } from "../../config/firebase";
+import { motion } from "framer-motion"
+import Header from "../header";
 
 export default function Landing() {
   const [loading, setLoading] = useState(true);
+  const [isTrue, setIsTrue] = useState(false);
   // const getData = () => {
   //   db.collection("test")
   //     .doc("mytestdoc")
@@ -20,8 +23,22 @@ export default function Landing() {
 
   return (
     <div>
-      <Link to="/about">About</Link>
-      {/* {loading && "Loading....."} */}
-    </div>
+      <Header />
+      {/* <motion.div className="one"
+          onClick={() => setIsTrue(!isTrue)}
+          style={{
+            borderRadius: 16
+          }}
+          animate={{
+            x: isTrue ? '80vw' : 0,
+            rotate: isTrue ? 180 : 0,
+          }}
+          transition={{
+            type: 'spring',
+            stiffness: 80,
+          }}
+        >
+        </motion.div>  */}
+    </div >
   );
 }
